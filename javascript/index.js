b98eccd71808b959aa8ce79041a5362901aed42b
venkateshkurugonda <venkateshkurugonda5@gmail.com>
2021-12-30 12:30:10 +0530

    all files are updated

---
+++ javascript/index.js
@@ -0,0 +1,53 @@
+// let name="venky";
+// let age=20;
+ 
+
+// if(age>18){
+//     name="venky k";
+//     console.log(name);
+
+// }
+// console.log(name);
+// let employee={
+//         name:"govardhan",
+//         designation:"junior software developer",
+//         salary:"6LPA",
+//         mobile:9392397104
+
+//     }
+//     let {name,designation,...remain}=employee;
+//     console.log(name);
+//     console.log(designation);
+//     console.log(remain);
+//     console.log(employee);
+    // console.log(employee)
+    // let employees=["venky","sadhak","gova"]
+    // console.log(employees[0])
+    //  alert("check")
+    //  let number1=prompt("enter number1")
+    //  console.log(number1)
+    //   confirm("are you sure to exit")
+    // console.log("welcome");
+    // console.info("for information");
+    // console.warn("check the code once");
+    // console.error("ok")
+        // let internal_marks=[24,75,94,32,745,74]
+        // let exteernal_marks=[54,86,35,32,86,45]
+        // let result=[23,34,exteernal_marks]
+        // let final=[96,354,34,...exteernal_marks,...
+        // internal_marks]
+        // let value=[internal_marks,exteernal_marks]
+        // console.log(final)
+        // console.log(result)
+        // console.log(value)
+        // function addition(x,y,...remainData){
+        //     console.log(x);
+        //     console.log(y);
+        //     console.log(typeof(remainData))
+        // }
+        // console.log(addition(2,3,45,35,75,2))
+        // let employees=["venky","gova","naveen","sadhak"]
+        // let [first,second,...data]=employees
+        // console.log(first)
+        // console.log(second)
+        // console.log(data)
